f34724d5812d528252dd4aeca1adc7ae0655cab8
JOHNSON JOSEPH <johnsonjoseph@JOHNSONs-MacBook-Air.local>
2019-07-20 17:04:10 -0700

    My Building & My Product Page works, all product & building controllers works

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,44 +1,65 @@
 class BuildingsController < ApplicationController
-    def index 
-        #if it's nested 
-        if params[:product_id] && @product = Product.find_by_id(params[:product_id])
-            @buildings = @product.buildings
-            #load up only the buildings nested under that product 
-        else
-            #go to next step
-            @buildings = Building.all
-       # @building = current_user.buildings #View buildings current_user is assigned to.
-    end 
-end
-
-    def new 
-         if params[:product_id] && @product = Product.find_by_id(params[:product_id])
-            #binding.pry
-            @building = @product.buildings.build(building_params)#has_many
-           
-        else
-            @building = Building.new
-            @building.build_product #belongs_to - nested form
-    end 
-end
     
-    def create 
-        if params[:product_id] && @product = Product.find_by_id(params[:product_id])
-            @building = @product.buildings.build(building_params)
-           else
-            @building = Building.new(building_params)
-           end
-      
-           if @building.save
-            redirect_to building_path(@building)
-           else
-            render :new
-           end
-        end
+    def index
+        @buildings = Building.all
+        @user_buildings = current_user.buildings
+        #binding.pry
+    end
 
+    def new
+        @building = Building.new
+    end
+
+    def create 
+            @building = current_user.buildings.build(building_params)
+        #binding.pry
+        if  @building.valid?
+                @building.save
+                flash[:success] = "Building Successfully Created!"
+                session[:building_id] = @building.id 
+                redirect_to building_path(@building)
+        else 
+            render :new
+        end 
+    end 
+
+    def show 
+        @building = Building.find(params[:id])
+    end 
 
     def building_params
-        params.require(:building).permit(:name, :product_id, product_attributes: [:name, :category, :description, :date_added, :user_id, :building_id])
+        params.require(:building).permit(:name)
     end 
   
 end
+    
+     
+    
+    
+
+    #def new 
+        # if params[:product_id] && @product = Product.find_by_id(params[:product_id])
+            #binding.pry
+           # @building = @product.buildings.build(building_params)#has_many
+           
+       # else
+           # @building = Building.new
+           # @building.build_product #belongs_to - nested form
+    #end 
+#end
+    
+    #def create 
+        #if params[:product_id] && @product = Product.find_by_id(params[:product_id])
+           # @building = @product.buildings.build(building_params)
+          # else
+          #  @building = Building.new(building_params)
+          # end
+          #if @building.save
+           # redirect_to building_path(@building)
+          # else
+           # render :new
+          # end
+       # end
+
+
+  
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,33 +1,70 @@
 class ProductsController < ApplicationController
+    
+    #def index 
+        #if it's nested 
+        #if params[:building_id] && @building = Building.find_by_id(params[:building_id])
+            #@user_products = @building.products
+            #load up only the buildings nested under that product 
+        #else
+            #go to next step
+           # @products = Product.all
+        #@product = current_user.products #View buildings current_user is assigned to.
+    #end 
+#end
+    
+
+  # def new 
+         #if params[:building_id] && @building = Building.find_by_id(params[:building_id])
+            #binding.pry
+           # @product = @building.products.build(product_params)#has_many
+           
+        #else
+         
+           #@product = Product.new
+            #@product.build_building #belongs_to - nested form
+    #end 
+#end
+
+#def create 
+        #if params[:building_id] && @building = Building.find_by_id(params[:building_id])
+           #@product = @product.buildings.build(building_params)
+           #else
+            # @product = Product.new(product_params)
+           #end
+          
+           #if @product.save
+           # redirect_to product_path(@building)
+           #else
+            #render :new
+           #end
+        #end
+
+    
+    
     def index 
         @products = Product.all 
         @user_products = current_user.products
         #binding.pry
-
     end 
 
     def new 
         @product = Product.new
         @product.build_building #belongs_to relationship for the nested building in product form
-
     end 
 
     def create 
-        @product = current_user.products.build(product_params)
+            @product = current_user.products.build(product_params)
         #binding.pry
-            if @product.valid?
+         if  @product.valid?
                 @product.save
                 session[:product_id] = @product.id 
                 redirect_to product_path(@product)
-            else 
+        else 
             render :show
+        end 
+     end 
 
-            end 
-    end 
 
-#def show 
-   #@product = Product.find_by(id: params[:id])
-#end 
 
 def show 
     @product = Product.find(params[:id])
