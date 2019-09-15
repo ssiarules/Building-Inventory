d5cadaf49c0bc3661a112fc15a2fcc6f44407de5
JOHNSON JOSEPH <johnsonjoseph@JOHNSONs-MacBook-Air.local>
2019-09-14 23:41:30 -0700

    Created new index, new create, new show, new update with json in building controller, cleaned code in application.html, session new page removed home path

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -2,34 +2,50 @@ class BuildingsController < ApplicationController
     
     def index
         @buildings = Building.all
-        @user_buildings = current_user.buildings
-        
-    end
+
+        render json: @notes, status: 200
+    end 
+    
+    
+    #def index
+      #  @buildings = Building.all
+       # @user_buildings = current_user.buildings
+    # end
 
     def new
         @building = Building.new
     end
 
     def create 
-            @building = current_user.buildings.build(building_params)
-        if  @building.save
-                flash[:success] = "Building Successfully Created!"
-                redirect_to building_path(@building)
-        else 
-            render :new
-        end 
+        @building = Building.create(building_params)
+
+        render json: @building, status: 200 
     end 
+    
+    
+    
+    #def create 
+           # @building = current_user.buildings.build(building_params)
+        #if  @building.save
+             #   flash[:success] = "Building Successfully Created!"
+              #  redirect_to building_path(@building)
+        #else 
+           # render :new
+        #end 
+    #end 
 
     def show 
         @building = Building.find(params[:id])
         render json: @building, status: 200
-
-        
         #@user_products = @building.products.ordered
     end 
 
+    private 
     def building_params
         params.require(:building).permit(:name)
     end 
+
+    
+   
   
 end
